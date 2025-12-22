c601794e5d59dc2c64f8120caf9727b1071cf920
vishalhit1 <vishal@handsintechnology.com>
2025-12-22 12:15:30 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './app.css'
+import './App.css'
 import './responsive.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from "@react-oauth/google";
